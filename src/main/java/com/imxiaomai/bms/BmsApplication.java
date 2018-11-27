9dc8629a3b99d5782e2efff5847ff2184aa1e5ca
于振京 <yuzhenjing@ydfinance.com.cn>
2018-11-27 18:01:48 +0800

    项目初始化

--- src/main/java/com/imxiaomai/bms/BmsApplication.java
+++ src/main/java/com/imxiaomai/bms/BmsApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 图书管理系统
+ */
 @SpringBootApplication
 @MapperScan("com.imxiaomai.bms.mapper")
 public class BmsApplication {
